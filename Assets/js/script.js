3e3893142a8d6004ec051f145b6fb789db46fd4d
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-26 12:23:33 -0400

    added clickButton logic for all buttons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,18 +1,122 @@
-function getCity(inputFromField){
-    var url="https://api.openweathermap.org/geo/1.0/direct?" + inputFromField + "&limit=1&appid=496722594d14437755a15609505942d8"
-    // fetch(url){
-    //     ...
-    // }
-    // .then(data){
-        data=[{"name":"Philadelphia","local_names":{"tr":"Filadelfiya","pl":"Filadelfia","oc":"Filadèlfia","ar":"فيلادلفيا","ja":"フィラデルフィア","pt":"Filadélfia","ko":"필라델피아","ur":"فلاڈیلفیا","en":"Philadelphia","fa":"فیلادلفیا","he":"פילדלפיה","es":"Filadelfia","fr":"Philadelphie","cs":"Filadelfie","be":"Філадэльфія","zh":"費城","ta":"பிலடெல்பியா","lv":"Filadelfija","uk":"Філадельфія","kn":"ಫಿಲಡೆಲ್ಫಿಯ","it":"Filadelfia","ru":"Филадельфия","ca":"Filadèlfia","hi":"फिलाडेल्फिया","te":"ఫిలడెల్ఫియా"},"lat":39.9527237,"lon":-75.1635262,"country":"US","state":"Pennsylvania"}]
-        var lat = data[0].lat;
-        var lon = data[0].lon;
-    console.log(lat + ", " + lon);
-    getWeatherCurrent(lat,lon);
-    // }
+var requestUrl = 'https://dog-api.kinduff.com/api/facts?number=13';
+//https://api.github.com/
+
+//https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=6180d0bfb66b0a537a54cd4b60470c4d
+var inputFromField1 = 'atlanta';
+var API_KEY = '6180d0bfb66b0a537a54cd4b60470c4d';
+
+let cityName = '';
+let searchBtn = '';
+
+// function getCurrentWeather(inputFromField);
+// {
+//     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputFromField}$&appid=${API_KEY}`;
+//     //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+//     fetch(requestUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//       const weatherData = `
+//         <p>Weather: ${data.weather[0].main}</p>
+//         <p>Description: ${data.weather[0].description}</p>
+//         <p>Temperature: ${data.main.temp} K</p>
+//         <p>Humidity: ${data.main.humidity}%</p>
+//       `;
+//       //document.getElementById('current-weather').innerHTML = weatherData; //replace jquery
+//     })
+//     .catch(error => console.error(error));
+// }
+
+// getCurrentWeather(inputFromField1);
+
+
+
+
+
+/**This function will get wether forecast for the city selcted by user from openweather API
+ * inputFromField (string) - city name entered by user as a search criteria
+ */
+// function getCity(inputFromField){
+//     // var inputFromField1 = 'atlanta';
+//     // var  API_KEY = '6180d0bfb66b0a537a54cd4b60470c4d';
+//     var url="https://api.openweathermap.org/geo/1.0/direct?q=" + inputFromField1 + "&limit=1&appid=6180d0bfb66b0a537a54cd4b60470c4" //use my appid
+//     // fetch(url){
+//     //     ...
+//     // }
+//     // .then(data){
+//         data=[{"name":"Philadelphia","local_names":{"tr":"Filadelfiya","pl":"Filadelfia","oc":"Filadèlfia","ar":"فيلادلفيا","ja":"フィラデルフィア","pt":"Filadélfia","ko":"필라델피아","ur":"فلاڈیلفیا","en":"Philadelphia","fa":"فیلادلفیا","he":"פילדלפיה","es":"Filadelfia","fr":"Philadelphie","cs":"Filadelfie","be":"Філадэльфія","zh":"費城","ta":"பிலடெல்பியா","lv":"Filadelfija","uk":"Філадельфія","kn":"ಫಿಲಡೆಲ್ಫಿಯ","it":"Filadelfia","ru":"Филадельфия","ca":"Filadèlfia","hi":"फिलाडेल्फिया","te":"ఫిలడెల్ఫియా"},"lat":39.9527237,"lon":-75.1635262,"country":"US","state":"Pennsylvania"}]
+//         var lat = data[0].lat;
+//         var lon = data[0].lon;
+//     console.log(lat + ", " + lon);
+//     getWeatherCurrent(lat,lon);
+//     // }
+// }
+// function getWeatherCurrent(lat, lon){
+
+// console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=496722594d14437755a15609505942d8`)
+// }
+
+
+
+
+/**
+ * This function  search city based on the user input and adding city button for predefined search
+ */
+function searchCity(button, city) {
+
+    console.log(button +'button clicked!' + city);
+    renderCityButton(button, city);
+    return;
 }
-function getWeatherCurrent(lat, lon){
-    
-console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=496722594d14437755a15609505942d8`)
+
+function init() {
+    // console.log('script is connected');
+    // Define click event handlers for each button
+    //ToDo - reads buttons from local storage
+
 }
-getCity("philadelphia")
+function renderCityButton (button, city) {
+    console.log('render city button');
+    return;
+}
+
+$(document).ready(function () {
+    init();
+
+
+    var cityBtnList = [];
+
+    $('#searchBtn').click(function () {
+        //console.log('search button is selected - 0');
+        searchBtn = $(this).attr('id');
+        cityName = $('#searchInput').val();
+        if (!cityName) return;
+        searchCity( searchBtn, cityName);
+        //ToDo - render new element in cityBtnList
+        cityBtnList.push(cityName);
+        console.log (cityBtnList);
+        renderCityButton();
+        localStorage.setItem('searchHistory', JSON.stringify(cityBtnList));
+        //ToDo - execute weather search
+    });
+    $('.list-group button').each(function () {
+        // 
+        
+        $(this).click(function () {
+            searchBtn = $(this).attr('id');
+            cityName = $(this).text();
+           // console.log($(this).text()+' button is selected');
+            searchCity(searchBtn, cityName);
+            //ToDo - render new element
+            //ToDo - execute weather search
+        });
+    //     // Get the ID of the button
+    //     var buttonID = $(this).attr('id');
+    //     // Get the text content of the button
+    //     var buttonText = $(this).text();
+    //     // Log the ID and text content of the button to the console
+    //     console.log('Button ID: ' + buttonID + ', Text Content: ' + buttonText);
+    //     searchCity(searchBtn, cityName);
+    });
+    return;
+});
+//getCity("philadelphia")
