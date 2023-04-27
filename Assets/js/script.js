b6f8c4c1577859692eda39e3fe8c76c49745ce6e
Inna Fedorenko <ivf.fedorenko@gmail.com>
2023-04-26 21:20:35 -0400

    added logic to get data from api

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,32 +2,61 @@ var requestUrl = 'https://dog-api.kinduff.com/api/facts?number=13';
 //https://api.github.com/
 
 //https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=6180d0bfb66b0a537a54cd4b60470c4d
-var inputFromField1 = 'atlanta';
+//https://api.openweathermap.org/data/2.5/forecast?q=Roma&appid=6180d0bfb66b0a537a54cd4b60470c4d&units=imperial
 var API_KEY = '6180d0bfb66b0a537a54cd4b60470c4d';
 
 let cityName = '';
 let searchBtn = '';
 var cityBtnList = [];
 
-// function getCurrentWeather(inputFromField);
-// {
-//     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputFromField}$&appid=${API_KEY}`;
-//     //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-//     fetch(requestUrl)
-//     .then(response => response.json())
-//     .then(data => {
-//       const weatherData = `
-//         <p>Weather: ${data.weather[0].main}</p>
-//         <p>Description: ${data.weather[0].description}</p>
-//         <p>Temperature: ${data.main.temp} K</p>
-//         <p>Humidity: ${data.main.humidity}%</p>
-//       `;
-//       //document.getElementById('current-weather').innerHTML = weatherData; //replace jquery
-//     })
-//     .catch(error => console.error(error));
-// }
+function getCurrentWeather(city) {
 
-// getCurrentWeather(inputFromField1);
+    // https://api.openweathermap.org/data/2.5/weather?q=Roma&appid=6180d0bfb66b0a537a54cd4b60470c4d
+    // https://api.openweathermap.org/data/2.5/weather?q=roma&appid=6180d0bfb66b0a537a54cd4b60470c4d
+
+
+    //console.log('Get current weather for the city: '+ city);
+    var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=imperial`
+    $.get(requestUrl, function (response) {
+        var currentCity= city+' - '
+       var currentDate = dayjs().format("MM/DD/YYYY");
+        var todayData = response.list[0];
+        var iconUrl = `https://openweathermap.org/img/w/${todayData.weather[0].icon}.png`;
+        var tempF = 'Temp: '+todayData.main.temp+ '°F';
+        var windSpeedMph = 'Wind: '+todayData.wind.speed+ 'MPH';
+        var humidity = 'Humidity: '+todayData.main.humidity +'%';
+
+        console.log('Citi is ' +city+' - ');
+        console.log('Data is ' +currentDate);
+        console.log('Icon is '+ iconUrl);
+        console.log('Temp is' + tempF);
+        console.log('Wind is'+windSpeedMph);
+        console.log('Humidity is '+humidity);
+
+        // Display the extracted data in the HTML
+        $("#nameCurrent").text(currentCity);
+        $("#dateCurrent").text(currentDate);
+        $("#imgCurrent").attr("src", iconUrl);
+        $("#tempCurrent").text(tempF);
+        $("#windCurrent").text(windSpeedMph);
+        $("#humidityCurrent").text(humidity);
+    });
+    //console.log(requestUrl);    
+    // fetch(requestUrl)
+    // .then(response => response.json())
+    // .then(data => {
+    //   const weatherData = `
+    //     <p>Weather: ${data.weather[0].main}</p>
+    //     <p>Description: ${data.weather[0].description}</p>
+    //     <p>Temperature: ${data.main.temp} K</p>
+    //     <p>Humidity: ${data.main.humidity}%</p>
+    //   `;
+    //   //document.getElementById('current-weather').innerHTML = weatherData; //replace jquery
+    // })
+    // .catch(error => console.error(error));
+}
+
+
 
 
 
@@ -63,8 +92,8 @@ var cityBtnList = [];
  * This function  search city based on the user input and adding city button for predefined search
  */
 function searchCity(button, city) {
-
     console.log(button + 'button clicked!' + city);
+    getCurrentWeather(city);
     // renderCityButton(button, city);
     return;
 }
@@ -82,14 +111,12 @@ function renderCityButton(city) {
     } else {
         console.log('User input does not match any array values.');
         var newButton = $('<button>', {
-            id: cityBtnList.length, 
+            id: cityBtnList.length,
             class: 'btn btn-secondary',
             text: city
         });
-
         // Append the new button element to the list-group div
         $('.list-group').append(newButton);
-        console.log('render city button');
         return;
         // Continue with the function
     }
@@ -98,23 +125,19 @@ function renderCityButton(city) {
 
 $(document).ready(function () {
     init();
-
-
-
-
     $('#searchBtn').click(function () {
         //console.log('search button is selected - 0');
         searchBtn = $(this).attr('id');
         cityName = $('#searchInput').val();
         if (!cityName) return;
-        //This function initiates weather search
-        searchCity(searchBtn, cityName);
         //This function adds new button with the city name from user search
         renderCityButton(cityName);
         // Update array with city names
         cityBtnList.push(cityName);
         // save city names to local storage
         localStorage.setItem('searchHistory', JSON.stringify(cityBtnList));
+        //This function initiates weather search
+        searchCity(searchBtn, cityName);
         // Clear user input
         cityName = '';
         $('#searchInput').val(cityName);
